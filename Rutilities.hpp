6c7930911be1f9d729e900c9c0070db3177cf2eb
whit <armstrong.whit@gmail.com>
2008-08-18 11:19:00 -0400

    dropped set/get colnamesMatrix from Rutilities.hpp since now implemented in Rmatrix.hpp

--- Rutilities.hpp
+++ Rutilities.hpp
@@ -49,46 +49,4 @@ void sexp2string(const SEXP str_sexp, T insert_iter) {
   }
 }
 
-template<typename T>
-void setColnamesMatrix(const SEXP x, T first, T last) {
-
-  SEXP dimnames, cnames;
-
-  const int cn_size = static_cast<const int>(std::distance(first,last));
-
-  if(cn_size!=ncols(x))
-    return;
-
-  PROTECT(cnames = string2sexp(first,last));
-  PROTECT(dimnames = allocVector(VECSXP, 2));
-  SET_VECTOR_ELT(dimnames, 0, R_NilValue);
-  SET_VECTOR_ELT(dimnames, 1, cnames);
-  setAttrib(x, R_DimNamesSymbol, dimnames);
-  UNPROTECT(2);
-}
-
-template<typename T>
-void getColnamesMatrix(const SEXP x, T insert_iter) {
-
-  SEXP dimnames, cnames;
-
-  PROTECT(dimnames = getAttrib(x, R_DimNamesSymbol));
-
-  if(dimnames==R_NilValue) {
-    UNPROTECT(1);  // dimnames
-    return;
-  }
-
-  PROTECT(cnames = VECTOR_ELT(dimnames, 1));
-
-  if(cnames==R_NilValue) {
-    UNPROTECT(2); // dimnames and cnames
-    return;
-  }
-
-  sexp2string(cnames,insert_iter);
-  UNPROTECT(2); // dimnames and cnames
-}
-
-
 #endif // RUTILTIES_HPP
